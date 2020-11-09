e70a31e27cb36b1b5cf5fd49b357717ab82198a3
serbyshek <66415549+serbyshek@users.noreply.github.com>
2020-11-09 19:12:02 +0300

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,8 @@
+a = float(input("Введите первое число"))
+b = float(input("Введите второе число"))
+c = float(input("Введите третье число"))
+d = float(input("Введите четвертое число"))
+sum1 = a + b
+sum2 = c + d
+rez = sum1/sum2
+print("% .2f" % rez)
---
+++ user.py
@@ -0,0 +1,4 @@
+username = input("Как вас зовут?")
+age = input("Сколько вам лет?")
+livein = input("Где вы живете?")
+print("Вас зовут {0}. Вам {1} лет. Вы живете в городе {2}.". format(username, age, livein))
